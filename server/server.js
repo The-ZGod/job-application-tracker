2dca64ee8254070e2b042a6e0dca8a91fbfc3fd8
Arihant <arihantalagoudar@gmail.com>
2026-09-08 00:58:51 +0530

    Enable CORS for client - server communication

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Job Application Tracker API is running!");
+app.get("/api/test", (req, res) => {
+    res.json({
+        message: "Hello from Node.js backend!",
+    });
 });
 
 app.listen(PORT, () => {
