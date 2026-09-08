620024131a03133e97a0d63e925cbbeec52c6bca
Arihant <arihantalagoudar@gmail.com>
2026-09-08 11:07:57 +0530

    Created Express test Route

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,13 @@
 import express from "express";
 import cors from "cors";
+import testRoutes from "./routes/test.routes.js"
 
 const app = express();
 
 app.use(cors());
+app.use("/api", testRoutes);
 
 const PORT = 5000;
-
-app.get("/api/test", (req, res) => {
-    res.json({
-        message: "Hello from Node.js backend!",
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
