01c33a3719fc232d5f7fdbdae55c6ad15ec0359e
Arihant <arihantalagoudar@gmail.com>
2026-09-09 22:46:20 +0530

    Added user database query

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,17 @@
 import express from "express";
+import prisma from "../lib/prisma.js"
 
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-    res.json({
-        message:"Users route is working!",
-    })
+router.get("/users", async (req, res) => {
+    try{
+        const users = await prisma.user.findMany();
+        res.json(users);
+    }catch(error){
+        res.status(500).json({
+            message: "Failed to fetch users!",
+        })
+    }
 })
 
 export default router;
