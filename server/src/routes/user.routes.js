c3005b22bff78cd4c379bd071df6dc9ae237c367
Arihant <arihantalagoudar@gmail.com>
2026-09-09 23:30:59 +0530

    Secure User registration using bcrypt

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import prisma from "../lib/prisma.js"
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -26,16 +27,31 @@ router.post("/users", async (req, res) => {
     const { name, email, password } = req.body;
 
     try{
+        //Hashing using bcrypt
+        //10 is bcrypt salt more salt, more strong password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const user = await prisma.user.create({
             data: {
                 name,
                 email,
-                password,
+                password: hashedPassword,
             },
         });
 
-        res.status(201).json(user);
+        res.status(201).json({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+        });
     }catch (error) {
+        if(error.code == "P2002"){
+            res.status(409).json({
+                message: "Email already exists!",
+            });
+        }
         res.status(500).json({
             message: "Failed to create user!",
         });
