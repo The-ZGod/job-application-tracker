ef19bddd1e1e2cd18c9e66885750b3daeda1c5f6
Arihant <arihantalagoudar@gmail.com>
2026-09-09 23:08:07 +0530

    Added create user and read APIs

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -5,13 +5,41 @@ const router = express.Router();
 
 router.get("/users", async (req, res) => {
     try{
-        const users = await prisma.user.findMany();
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+        });
         res.json(users);
     }catch(error){
         res.status(500).json({
             message: "Failed to fetch users!",
-        })
+        });
     }
-})
+});
+
+router.post("/users", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try{
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password,
+            },
+        });
+
+        res.status(201).json(user);
+    }catch (error) {
+        res.status(500).json({
+            message: "Failed to create user!",
+        });
+    }
+});
 
 export default router;
