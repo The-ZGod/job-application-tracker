39be631ccd51a248170fd297c8449e659ecfc3ec
Arihant <arihantalagoudar@gmail.com>
2026-09-12 01:29:38 +0530

    Added glass navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { NavLink, Routes, Route } from 'react-router-dom'
+import './App.css'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
@@ -6,12 +7,21 @@ import Applications from './pages/Applications'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/applications" element={<Applications />} />
-    </Routes>
+    <>
+      <nav>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/applications">Applications</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/register">Register</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/applications" element={<Applications />} />
+      </Routes>
+    </>
   )
 }
 
